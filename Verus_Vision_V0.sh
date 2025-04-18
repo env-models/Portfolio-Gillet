fda0ff45ae404d69def877c12a563c9db3db1b69
env-models <45174955+env-models@users.noreply.github.com>
2025-04-18 22:04:54 +0200

    Update Verus_Vision_V0.sh

--- Verus_Vision_V0.sh
+++ Verus_Vision_V0.sh
@@ -86,7 +86,7 @@ VAE_MODELS=(
     # "https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.safetensors"
     # "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
     # "https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"
-    "https://huggingface.co/SG161222/Verus_Vision_1.0b/resolve/main/ae.safetensors"
+    # "https://huggingface.co/SG161222/Verus_Vision_1.0b/resolve/main/ae.safetensors"
     "https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors"
     # "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors"
 )
